c1d02a388842ada3d70190a950be2fa6b7da41a8
Eminosoft <eminosoft@iMac-8s-iMac.local>
2014-04-15 16:28:33 +0530

    Initial Commit

---
+++ SampleRestaurantAp/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  SampleRestaurantAp
+//
+//  Created by Eminosoft on 4/15/14.
+//  Copyright (c) 2014 Eminosoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ SampleRestaurantAp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SampleRestaurantAp
+//
+//  Created by Eminosoft on 4/15/14.
+//  Copyright (c) 2014 Eminosoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
